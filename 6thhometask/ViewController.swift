bd2ffe1c60ce6b61cce7f81fa87603ffd7f30a9f
Aero <aero@gmail.com>
2024-09-11 15:56:23 +0500

    Initial Commit

---
+++ 6thhometask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  6thhometask
+//
+//  Created by vv 77 on 11/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
